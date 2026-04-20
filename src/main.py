b32911a0ae4db499aca9c67787b2386c3d9b2cb5
David Peacock <david.peacock@aluno.uece>
2026-04-20 12:14:33 -0300

    feat: add gameboy resolution and implement scale

--- src/main.py
+++ src/main.py
@@ -2,23 +2,29 @@ import sys
 import pygame
 
 from engine.SetPixel import setPixel
+from constants import PALETTE
 
 class Game:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Diamond Rush")
-        width, height = 800, 600
-        self.screen = pygame.display.set_mode((width, height))
+        GAME_W, GAME_H, SCALE = 160, 144, 4
+        self.WIN_W, self.WIN_H = GAME_W * SCALE, GAME_H * SCALE
+        self.window = pygame.display.set_mode((self.WIN_W, self.WIN_H))
+        self.screen = pygame.Surface((GAME_W, GAME_H))
 
     def run(self):
         running = True
         while running:
+            scaled = pygame.transform.scale(self.screen, (self.WIN_W, self.WIN_H))
+            self.window.blit(scaled, (0, 0))
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-
                     
-            pygame.display.update()
+            pygame.display.flip()
+
 
 if __name__ == "__main__":
     game = Game()
