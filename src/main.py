ef72e1b19fa6b79117a5311adac77b443785391d
HumDavid <david.peacock@aluno.uece.br>
2026-04-27 00:34:06 -0300

    add all sprites of the game

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ class Game:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Diamond Rush")
-        GAME_W, GAME_H, SCALE = 160, 144, 6
+        GAME_W, GAME_H, SCALE = 160, 144, 5
         self.WIN_W, self.WIN_H = GAME_W * SCALE, GAME_H * SCALE
         self.window = pygame.display.set_mode((self.WIN_W, self.WIN_H))
         self.window = pygame.display.set_mode((self.WIN_W, self.WIN_H))
