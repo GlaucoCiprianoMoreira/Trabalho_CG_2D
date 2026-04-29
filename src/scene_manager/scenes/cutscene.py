45382545d2cccc5985e62e29be7acd579731fc0f
HumDavid <david.peacock@aluno.uece.br>
2026-04-28 22:02:59 -0300

    fix: remove blit and fill functions

--- src/scene_manager/scenes/cutscene.py
+++ src/scene_manager/scenes/cutscene.py
@@ -11,7 +11,6 @@ class CutsceneScene(Scene):
     def __init__(self, manager):
         self.manager = manager
         self.n_frame = 1
-        self._bg_cache = pygame.Surface((160, 144))
     
     def handle_event(self, event):
         #n_frame = 0
@@ -23,13 +22,8 @@ class CutsceneScene(Scene):
             self.manager.go_to("gameplay")
     
     def draw(self, screen):
-        screen.fill((0, 0, 0))
-        self._bg_cache.fill((0, 0, 0))
-
         path = "assets/cutscene/frame-scene-0" + str(self.n_frame) + ".png"
         matrix = load_png_matrix(path)
-        draw_sprite(self._bg_cache, matrix, 0, 0)
+        draw_sprite(screen, matrix, 0, 0)
 
-        draw_text(self._bg_cache, "SPACE I ENTER", 80, 10, LIGHTEST, 1, mode="center")
-
-        screen.blit(self._bg_cache, (0, 0))  # cola o fundo
+        draw_text(screen, "SPACE I ENTER", 80, 10, LIGHTEST, 1, mode="center")
