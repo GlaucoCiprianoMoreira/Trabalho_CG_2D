6d3616eb082ce30bc0a6aab66d1e84a933278c1c
HumDavid <david.peacock@aluno.uece.br>
2026-05-02 16:09:35 -0300

    add all audios and implement start the music implementation

--- src/scene_manager/scenes/cutscene.py
+++ src/scene_manager/scenes/cutscene.py
@@ -1,4 +1,5 @@
 import pygame
+import audio_manager
 
 from engine.HUD.text import draw_text
 from engine.LoadMatrix import load_png_matrix, draw_sprite
@@ -10,6 +11,7 @@ from scene_manager.scene import Scene
 class CutsceneScene(Scene):
     def __init__(self, manager):
         self.manager = manager
+        audio_manager.play_music('space-ii-cutscene')
         self.n_frame = 1
     
     def handle_event(self, event):
@@ -22,7 +24,7 @@ class CutsceneScene(Scene):
             self.manager.go_to("gameplay")
     
     def draw(self, screen):
-        path = "assets/cutscene/frame-scene-0" + str(self.n_frame) + ".png"
+        path = "assets/sprites/cutscene/frame-scene-0" + str(self.n_frame) + ".png"
         matrix = load_png_matrix(path)
         draw_sprite(screen, matrix, 0, 0)
 
--- src/scene_manager/scenes/gameplay.py
+++ src/scene_manager/scenes/gameplay.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 
+import audio_manager
 from scene_manager.scene import Scene
 from constants import load_tiles
 from loader.LoadMap import draw_level, levels
@@ -10,6 +11,7 @@ from mechanics.Player import Player
 class GameplayScene(Scene):
     def __init__(self, manager):
         self.manager = manager
+        audio_manager.play_music('resonance')
         self.tiles = load_tiles()
         self.level = random.choice(levels)
 
@@ -17,14 +19,14 @@ class GameplayScene(Scene):
         self.GAME_H = 144
 
         player_sprites = {
-            "up": [load_png_matrix("assets/player/player_tras1.png"), load_png_matrix("assets/player/player_tras2.png"),
-                     load_png_matrix("assets/player/player_tras3.png"), load_png_matrix("assets/player/player_tras4.png")],
-            "down": [load_png_matrix("assets/player/player_frente1.png"), load_png_matrix("assets/player/player_frente2.png"),
-                    load_png_matrix("assets/player/player_frente3.png"), load_png_matrix("assets/player/player_frente4.png")],
-            "left": [load_png_matrix("assets/player/player_esquerda1.png"), load_png_matrix("assets/player/player_esquerda2.png"),
-                     load_png_matrix("assets/player/player_esquerda3.png"), load_png_matrix("assets/player/player_esquerda4.png")],
-            "right": [load_png_matrix("assets/player/player_direita1.png"), load_png_matrix("assets/player/player_direita2.png"),
-                      load_png_matrix("assets/player/player_direita3.png"), load_png_matrix("assets/player/player_direita4.png")],
+            "up": [load_png_matrix("assets/sprites/player/player_tras1.png"), load_png_matrix("assets/sprites/player/player_tras2.png"),
+                     load_png_matrix("assets/sprites/player/player_tras3.png"), load_png_matrix("assets/sprites/player/player_tras4.png")],
+            "down": [load_png_matrix("assets/sprites/player/player_frente1.png"), load_png_matrix("assets/sprites/player/player_frente2.png"),
+                    load_png_matrix("assets/sprites/player/player_frente3.png"), load_png_matrix("assets/sprites/player/player_frente4.png")],
+            "left": [load_png_matrix("assets/sprites/player/player_esquerda1.png"), load_png_matrix("assets/sprites/player/player_esquerda2.png"),
+                     load_png_matrix("assets/sprites/player/player_esquerda3.png"), load_png_matrix("assets/sprites/player/player_esquerda4.png")],
+            "right": [load_png_matrix("assets/sprites/player/player_direita1.png"), load_png_matrix("assets/sprites/player/player_direita2.png"),
+                      load_png_matrix("assets/sprites/player/player_direita3.png"), load_png_matrix("assets/sprites/player/player_direita4.png")],
         }
         
         self.player = Player(16, 16, player_sprites)
